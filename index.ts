15aae2644e59ce92f75f60e4ceb5362853fea0ee
Ramon Silveira Borges <ramonborges@Ramons-Mac-mini.local>
2022-11-20 02:08:16 +0100

    feat: vpc definition done

--- index.ts
+++ index.ts
@@ -1,9 +1,39 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+export interface vpcInterface {
+    name: string;
+    cidrBlock: string;
+    enableDnsHostname: boolean;
+    tags?: {};
+}
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const defaultTags: {} = {
+    Package: "groundwork_aws"
+}
+
+function createVpc(vpcData: vpcInterface): aws.ec2.Vpc {
+
+    let vpcTags: {} = {};
+    if(vpcData.tags == undefined){
+        vpcTags = defaultTags
+    }
+    else{
+        vpcTags = Object.assign(vpcData.tags, defaultTags);
+    }
+    
+    const vpc = new aws.ec2.Vpc(vpcData.name, {
+        cidrBlock: vpcData.cidrBlock,
+        instanceTenancy: "default",
+        enableDnsHostnames: vpcData.enableDnsHostname,
+        tags: vpcTags,
+    });
+
+    return vpc
+}
+
+export function createGroundWork(vpcData: vpcInterface) {
+    const mainVpc = createVpc(vpcData);
+}
+
+createGroundWork({name: "iac-vpc", cidrBlock: "10.0.0.0/16", enableDnsHostname: true, tags: {Name: "iac-vpc"}})
